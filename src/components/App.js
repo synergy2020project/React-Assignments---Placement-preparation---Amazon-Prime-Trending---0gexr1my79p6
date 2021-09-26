d050efff09d766cd3d8af836d9d6175ba9ac0d7d
Prashant Kumar Soni <project2020synergy@gmail.com>
2021-09-26 09:26:42 -0600

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import slides from "../data";
 
 const App = () => {
-  return (
-    <></>
-  )
-}
+  const [index, setIndex] = useState(0);
 
+  return (
+    <>
+      <div id="slide">
+        <h1 data-testid="title">{slides[index].title}</h1>
+        <p data-testid="text">{slides[index].text}</p>
+      </div>
+      <div id="navigation">
+        <button
+          disabled={index === 0}
+          data-testid="button-prev"
+          onClick={() => {
+            setIndex((index) => index - 1);
+          }}
+        >
+          Prev
+        </button>
+        <button
+          disabled={index === 0}
+          data-testid="button-restart"
+          onClick={() => {
+            setIndex(0);
+          }}
+        >
+          Restart
+        </button>
+        <button
+          disabled={index === slides.length - 1}
+          data-testid="button-next"
+          onClick={() => {
+            setIndex((index) => index + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default App;
